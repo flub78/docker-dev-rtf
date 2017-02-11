822002d173026e2238188d20ed934619b3e850fc
Frédéric <frederic.peignot@free.fr>
2017-02-11 12:09:19 +0100

    Creation of few shortcut scripts

---
+++ restart.sh
@@ -0,0 +1,1 @@
+docker restart dev-rtf
--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-docker run -ti --name=dev-rtf -e DISPLAY=$DISPLAY -v $HOME/.ssh/:/root/.ssh/ -v /tmp/.X11-unix:/tmp/.X11-unix flub78/dev-rtf /bin/bash
+docker run -ti --name=dev-rtf -e DISPLAY=$DISPLAY -v $HOME/.ssh/:/root/.ssh/ -v /tmp/.X11-unix:/tmp/.X11-unix -p 10080:80 flub78/dev-rtf /bin/bash
